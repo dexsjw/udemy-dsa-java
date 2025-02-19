53d8a42044f62fca2cf9b2e41f512ef301f845e4
Soh <dexsjw@gmail.com>
2025-02-19 18:34:04 +0800

    completed Quick Sort lesson

--- SortAlgorithms/QuickSort/src/Main.java
+++ SortAlgorithms/QuickSort/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     /*
         Characteristics of Quick Sort:
@@ -37,27 +39,135 @@ public class Main {
         quickSort(input, pivotIndex + 1, end);
     }
 
+    // Initial Implementation
+//    public static int partition(int[] input, int start, int end) {
+//        // This is using the first element as the pivot
+//        int pivot = input[start];
+//        int i = start;
+//        int j = end;
+//
+//        while (i < j) {
+//            System.out.println("j: " + j);
+//            // NOTE: empty loop body
+//            while (i < j && input[--j] >= pivot);
+//            if (i < j) {
+//                input[i] = input[j];
+//            }
+//
+//            System.out.println("i: " + i);
+//            // NOTE: empty loop body
+//            while (i < j && input[++i] <= pivot);
+//            if (i < j) {
+//                input[j] = input[i];
+//            }
+//        }
+//
+//        input[j] = pivot;
+//        System.out.println("i: " + i + ", j: " + j);
+//        System.out.println(Arrays.toString(input));
+//        return j;
+//
+////        input[i] = pivot;
+////        System.out.println("i: " + i + ", j: " + j);
+////        System.out.println(Arrays.toString(input));
+////        return i;
+//
+////        Returning "i" or "j" will have no difference for this implementation
+////        -> because "--j" or "++i" is in the "while" loop conditional check
+////        -> the increment or decrement will not occur once i == j
+////        -> i == j always true when exiting outer "while" loop
+//    }
+
+    // Alternate Implementation 1
+//    public static int partition(int[] input, int start, int end) {
+//        // This is using the first element as the pivot
+//        int pivot = input[start];
+//        int i = start;
+//        int j = end;
+//
+//        while (i < j) {
+//            System.out.println("j: " + j);
+//            --j;
+//            while (i < j && input[j] >= pivot) {
+//                j--;
+//            }
+//            if (i < j) {
+//                input[i] = input[j];
+//            }
+//
+//            System.out.println("i: " + i);
+//            ++i;
+//            while (i < j && input[i] <= pivot) {
+//                i++;
+//            }
+//            if (i < j) {
+//                input[j] = input[i];
+//            }
+//        }
+//
+//        input[j] = pivot;
+//        System.out.println("i: " + i + ", j: " + j);
+//        System.out.println(Arrays.toString(input));
+//        return j;
+//
+////        input[i] = pivot;
+////        System.out.println("i: " + i + ", j: " + j);
+////        System.out.println(Arrays.toString(input));
+////        return i;
+//
+////        Returning "i" or "j" will have different results for this implementation
+////        -> because the "--j" or "++i" is outside the "while" loop conditional check
+////        -> the increment or decrement will always occur
+////        -> i == j NOT always true when exiting outer "while" loop
+////        -> always select the first incremented / decremented index to return i.e. "j" in this case
+//    }
+
+    // Alternate Implementation 2
     public static int partition(int[] input, int start, int end) {
         // This is using the first element as the pivot
         int pivot = input[start];
         int i = start;
-        int j = end;
+        int j = end - 1;
 
         while (i < j) {
-            // NOTE: empty loop body
-            while (i < j && input[--j] >= pivot);
+            System.out.println("j: " + j);
+            while (i < j && input[j] >= pivot) {
+                --j;
+            }
             if (i < j) {
                 input[i] = input[j];
             }
 
-            // NOTE: empty loop body
-            while (i < j && input[++i] <= pivot);
+            System.out.println("i: " + i);
+            while (i < j && input[i] <= pivot) {
+                ++i;
+            }
             if (i < j) {
                 input[j] = input[i];
             }
         }
 
         input[j] = pivot;
+        System.out.println("i: " + i + ", j: " + j);
+        System.out.println(Arrays.toString(input));
         return j;
+
+//        input[i] = pivot;
+//        System.out.println("i: " + i + ", j: " + j);
+//        System.out.println(Arrays.toString(input));
+//        return i;
+
+//        Returning "i" or "j" will have no difference for this implementation
+//        -> because "--j" or "++i" is inside the "while" loop
+//        -> the increment or decrement will not occur once i == j
+//        -> i == j always true when exiting outer "while" loop
+//        Note:
+//        - This implementation will still check for the elements in the replaced position
+//        - However, it should not have any impact because the replaced position would have already been checked for
+//          greater / lesser than the pivot element value
+//        - e.g.
+//          For the first iteration,
+//          after -22 has been placed in index 0, -22 will be checked again
+//          after  35 has been placed in index 6,  35 will be checked again
     }
 }
